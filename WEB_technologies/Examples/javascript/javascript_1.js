1f0b72a19734582c112ee6e7094af23acf1abf03
Juan Carlos <carlos_isc7@outlook.com>
2018-05-10 17:08:27 -0500

    examples using javascript seen in classes

---
+++ WEB_technologies/Examples/javascript/javascript_1.js
@@ -0,0 +1,84 @@
+
+
+
+/*
+var hm = "Hola Mundo";
+var hmM = hm.substring(3,8);
+
+var num = 3.1416;
+
+var res = hm + num;
+
+*/
+
+
+
+function suma(op1,op2){
+    if(op1+op2 > 10)
+       alert("ÑOÑO");
+    else
+        alert("YES");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var persona = {
+    nombre:"Juan",
+    apellido:"Pérez",
+    id:64,
+    nombreCompleto:function(){
+        return this.nombre+" "+this.apellido;
+    }
+}
+
+alert(persona.nombreCompleto());
+*/
